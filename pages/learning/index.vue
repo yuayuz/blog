75ab57d265ecb7b3077bf0ea1736fbd00c57fbd1
yu <2665764951@qq.com>
2024-09-08 11:00:44 +0800

    add pages

---
+++ pages/learning/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tw-text-center">learning</div>
+</template>
+
+<script setup lang="ts">
+import { definePageMeta } from "#imports";
+
+definePageMeta({
+  layout: "nav-layout",
+});
+</script>
+
+<style scoped></style>
---
+++ pages/reading/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tw-text-center">reading</div>
+</template>
+
+<script setup lang="ts">
+import { definePageMeta } from "#imports";
+
+definePageMeta({
+  layout: "nav-layout",
+});
+</script>
+
+<style scoped></style>
