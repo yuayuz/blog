a25ba27e5587befcc32632d883fd916f184ccab2
yu <2665764951@qq.com>
2025-05-16 22:03:47 +0800

    添加主页

--- app.vue
+++ app.vue
@@ -1,7 +1,3 @@
 <template>
-  <v-app>
-    <div class="tw:justify-center tw:h-screen tw:items-center tw:flex">
-      <p class="tw:mx-auto tw:w-min">hello,blog!!!</p>
-    </div>
-  </v-app>
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="tw:flex tw:justify-center tw:items-center tw:h-screen">
+    <h1
+      class="tw:text-[clamp(4rem,10vw,8rem)] tw:font-extrabold tw:text-gray-400 tw:transition-all tw:duration-500 tw:ease-in-out tw:hover:text-transparent tw:hover:bg-clip-text tw:hover:bg-gradient-to-r tw:hover:from-pink-500 tw:hover:via-yellow-400 tw:hover:to-blue-500"
+    >
+      谁是谁
+    </h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style></style>
