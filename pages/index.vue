d627fb01947d25f041a14a7d9590d23b366cb823
yu <2665764951@qq.com>
2025-05-16 22:19:37 +0800

    取消tw前缀

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="tw:flex tw:justify-center tw:items-center tw:h-screen">
+  <div class="flex h-screen items-center justify-center">
     <h1
-      class="tw:text-[clamp(4rem,10vw,8rem)] tw:font-extrabold tw:text-gray-400 tw:transition-all tw:duration-500 tw:ease-in-out tw:hover:text-transparent tw:hover:bg-clip-text tw:hover:bg-gradient-to-r tw:hover:from-pink-500 tw:hover:via-yellow-400 tw:hover:to-blue-500"
+      class="text-[clamp(4rem,10vw,8rem)] font-extrabold text-gray-400 transition-all duration-500 ease-in-out hover:bg-gradient-to-r hover:from-pink-500 hover:via-yellow-400 hover:to-blue-500 hover:bg-clip-text hover:text-transparent"
     >
       谁是谁
     </h1>
