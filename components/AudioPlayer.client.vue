b0aecb3772fb7c9b11091c6592fc544f346196e2
yu <2665764951@qq.com>
2025-06-11 19:45:30 +0800

    解决 audioPlayer 服务器渲染问题

--- components/AudioPlayer.client.vue
+++ components/AudioPlayer.client.vue
@@ -1,31 +1,36 @@
 <template>
-  <div id="aplayer" class="mt-4" />
+  <ClientOnly>
+    <div id="aplayer" class="mt-4" />
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
-import 'aplayer/dist/APlayer.min.css'
-// @ts-ignore 忽略类型报错（APlayer 无类型定义）
-import APlayer from 'aplayer'
+if (import.meta.client) {
+  // @ts-ignore
+  import('aplayer').then(({ default: APlayer }) => {
+    import('aplayer/dist/APlayer.min.css')
 
-onMounted(() => {
-  const ap = new APlayer({
-    container: document.getElementById('aplayer')!,
-    fixed: true,
-    autoplay: false,
-    audio: [
-      {
-        name: 'La gloire à mes genoux',
-        artist: 'Artist',
-        url: '/music/Le_rouge_et_le_noir/La gloire à mes genoux.mp3',
-        cover: '/music/Le_rouge_et_le_noir/Le_rouge_et_le_noir.png',
-      },
-      {
-        name: 'Ding Dong',
-        artist: 'Artist',
-        url: '/music/Le_rouge_et_le_noir/Ding Dong.mp3',
-        cover: '/music/Le_rouge_et_le_noir/Le_rouge_et_le_noir.png',
-      },
-    ],
+    onMounted(() => {
+      const ap = new APlayer({
+        container: document.getElementById('aplayer')!,
+        fixed: true,
+        autoplay: false,
+        audio: [
+          {
+            name: 'La gloire à mes genoux',
+            artist: 'Artist',
+            url: '/music/Le_rouge_et_le_noir/La gloire à mes genoux.mp3',
+            cover: '/music/Le_rouge_et_le_noir/Le_rouge_et_le_noir.png',
+          },
+          {
+            name: 'Ding Dong',
+            artist: 'Artist',
+            url: '/music/Le_rouge_et_le_noir/Ding Dong.mp3',
+            cover: '/music/Le_rouge_et_le_noir/Le_rouge_et_le_noir.png',
+          },
+        ],
+      })
+    })
   })
-})
+}
 </script>
